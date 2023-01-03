18309846f980a266fa74d6b4d573838f1945ba75
Thomas Beranek <github@beranek.one>
2023-01-03 18:48:46 +0000

    Use ginads for ADS backend requests.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	ads "github.com/beranek1/ads-bridge-go-lib"
+	"github.com/beranek1/ginads"
 	"github.com/beranek1/goconfig"
 	"github.com/beranek1/godata"
 	"github.com/gin-gonic/gin"
@@ -17,121 +18,26 @@ var adsBridge ads.ADSBridge
 var configManager goconfig.ConfigManager
 var dataManager godata.DataManager
 
-func returnADSResult(c *gin.Context, dat map[string]interface{}, err error) {
-	if err != nil {
-		c.String(500, "{\"error\":\""+err.Error()+"\"}")
-	} else {
-		byt, err := json.Marshal(dat)
-		if err != nil {
-			c.String(500, "{\"error\":\""+err.Error()+"\"}")
-		} else {
-			c.String(200, string(byt))
-		}
-	}
-}
+var adsBackend *ginads.Backend[map[string]interface{}]
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 	r.SetTrustedProxies(nil)
-	r.GET("/ads/version", func(c *gin.Context) {
-		dat, err := adsBridge.GetVersion()
-		returnADSResult(c, dat, err)
-	})
+	r.GET("/ads/version", adsBackend.GetVersion)
 
-	r.GET("/ads/state", func(c *gin.Context) {
-		dat, err := adsBridge.GetState()
-		returnADSResult(c, dat, err)
-	})
+	r.GET("/ads/state", adsBackend.GetState)
 
-	r.POST("/ads/state", func(c *gin.Context) {
-		rawData, err := c.GetRawData()
-		if err == nil {
-			var data map[string]interface{}
-			err := json.Unmarshal(rawData, &data)
-			if err == nil {
-				adsStateData, hasADS := data["adsState"]
-				deviceStateData, hasDevice := data["deviceState"]
-				if hasADS {
-					if val1, ok1 := adsStateData.(uint16); ok1 {
-						adsState := uint16(val1)
-						if hasDevice {
-							if val2, ok2 := deviceStateData.(uint16); ok2 {
-								deviceState := uint16(val2)
-								dat, err := adsBridge.WriteControl(adsState, deviceState)
-								returnADSResult(c, dat, err)
-							} else {
-								c.String(400, "{\"error\":\"Failed converting deviceState.\"}")
-							}
-						} else {
-							dat, err := adsBridge.WriteControl(adsState, 0)
-							returnADSResult(c, dat, err)
-						}
-					} else {
-						c.String(400, "{\"error\":\"Failed converting adsState.\"}")
-					}
-				} else if hasDevice {
-					if val1, ok1 := deviceStateData.(uint16); ok1 {
-						deviceState := uint16(val1)
-						dat, err := adsBridge.WriteControl(0, deviceState)
-						returnADSResult(c, dat, err)
-					} else {
-						c.String(400, "{\"error\":\"Failed converting deviceState.\"}")
-					}
-				} else {
-					dat, err := adsBridge.GetState()
-					returnADSResult(c, dat, err)
-				}
-			} else {
-				c.String(500, "{\"error\":\""+err.Error()+"\"}")
-			}
-		} else {
-			c.String(500, "{\"error\":\""+err.Error()+"\"}")
-		}
-	})
+	r.POST("/ads/state", adsBackend.WriteControl)
 
-	r.GET("/ads/deviceInfo", func(c *gin.Context) {
-		dat, err := adsBridge.GetDeviceInfo()
-		returnADSResult(c, dat, err)
-	})
+	r.GET("/ads/deviceInfo", adsBackend.GetDeviceInfo)
 
-	r.GET("/ads/symbolInfo/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		dat, err := adsBridge.GetSymbolInfo(name)
-		returnADSResult(c, dat, err)
-	})
+	r.POST("/ads/symbolList", adsBackend.ListSymbols)
 
-	r.GET("/ads/symbolValue/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		dat, err := adsBridge.GetSymbolValue(name)
-		returnADSResult(c, dat, err)
-	})
+	r.GET("/ads/symbolInfo/:name", adsBackend.GetSymbolInfo)
 
-	r.POST("/ads/symbolValue/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		rawData, err := c.GetRawData()
-		if err == nil {
-			var data map[string]interface{}
-			err := json.Unmarshal(rawData, &data)
-			if err == nil {
-				if value, exists := data["data"]; exists {
-					jsonValue, err := json.Marshal(value)
-					if err == nil {
-						dat, err := adsBridge.SetSymbolValue(name, string(jsonValue))
-						returnADSResult(c, dat, err)
-					} else {
-						c.String(500, "{\"error\":\""+err.Error()+"\"}")
-					}
-				} else {
-					dat, err := adsBridge.GetSymbolValue(name)
-					returnADSResult(c, dat, err)
-				}
-			} else {
-				c.String(500, "{\"error\":\""+err.Error()+"\"}")
-			}
-		} else {
-			c.String(500, "{\"error\":\""+err.Error()+"\"}")
-		}
-	})
+	r.GET("/ads/symbolValue/:name", adsBackend.GetSymbolValue)
+
+	r.POST("/ads/symbolValue/:name", adsBackend.SetSymbolValue)
 
 	r.GET("/config/:name", func(c *gin.Context) {
 		name := c.Param("name")
@@ -198,6 +104,7 @@ func main() {
 	if err != nil {
 		println("Error: Specified ADSBridge unavailable due to error: ", err.Error())
 	}
+	adsBackend = ginads.Create[map[string]interface{}](adsBridge)
 
 	configManager, err = goconfig.Manage("config")
 	if err != nil {
